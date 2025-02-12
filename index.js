42b838f94d87344fb60b886d226083b86d57b473
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-02-12 14:29:32 +0100

    Refactoring Code

--- index.js
+++ index.js
@@ -1,60 +1,24 @@
 "use strict";
 
-//EVENT LISTENER WHEN CLICKING ON QUESTION
-const openFaqText = document.querySelectorAll(".question");
+const faqContentQuestion = document.querySelectorAll(".faq-content-question");
+faqContentQuestion.forEach((faqsBox) => {
+  const question = faqsBox.querySelector(".question");
+  const faqsBtn = faqsBox.querySelector(".toggle-faq-button");
 
-openFaqText.forEach((text) => {
-  text.setAttribute("tabindex", "0");
-  const imgBtn = text.nextElementSibling;
-  const showAnswer = text.parentElement.nextElementSibling;
-
-  text.addEventListener("click", () => {
-    if (showAnswer.classList.contains("hidden")) {
-      showAnswer.classList.remove("hidden");
-      imgBtn.src = "./assets/images/icon-minus.svg";
-    } else {
-      showAnswer.classList.add("hidden");
-      imgBtn.src = "./assets/images/icon-plus.svg";
-    }
+  question.addEventListener("click", () => {
+    toggleAnswer(question);
   });
 
-  //KEYBOARD EVENT
-  text.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && showAnswer.classList.contains("hidden")) {
-      showAnswer.classList.remove("hidden");
-      imgBtn.src = "./assets/images/icon-minus.svg";
-    } else {
-      showAnswer.classList.add("hidden");
-      imgBtn.src = "./assets/images/icon-plus.svg";
-    }
+  faqsBtn.addEventListener("click", () => {
+    toggleAnswer(faqsBtn);
   });
 });
 
-//EVENT LISTENER WHEN CLICKING ON IMAGE
-const openBtn = document.querySelectorAll(".toggle-faq-button");
-
-openBtn.forEach((imgBtn) => {
-  imgBtn.setAttribute("tabindex", "0");
-  const showAnswer = imgBtn.parentElement.nextElementSibling;
-
-  imgBtn.addEventListener("click", () => {
-    if (showAnswer.classList.contains("hidden")) {
-      showAnswer.classList.remove("hidden");
-      imgBtn.src = "./assets/images/icon-minus.svg";
-    } else {
-      showAnswer.classList.add("hidden");
-      imgBtn.src = "./assets/images/icon-plus.svg";
-    }
-  });
-
-  //KEYBOARD EVENT
-  imgBtn.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && showAnswer.classList.contains("hidden")) {
-      showAnswer.classList.remove("hidden");
-      imgBtn.src = "./assets/images/icon-minus.svg";
-    } else {
-      showAnswer.classList.add("hidden");
-      imgBtn.src = "./assets/images/icon-plus.svg";
-    }
-  });
-});
+function toggleAnswer(toggle) {
+  const showAnswer = toggle.parentElement.nextElementSibling;
+  if (showAnswer.classList.contains("hidden")) {
+    showAnswer.classList.remove("hidden");
+  } else {
+    showAnswer.classList.add("hidden");
+  }
+}
